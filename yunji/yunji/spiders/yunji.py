5bad9258e8c2298ebe92f90735dc29345f25474e
julis <617669559@qq.com>
2018-08-09 15:12:54 +0800

    julis
    test

--- yunji/yunji/spiders/yunji.py
+++ yunji/yunji/spiders/yunji.py
@@ -20,7 +20,6 @@ class Yunji(scrapy.Spider):
     # 测试数据
     cookie = {'acw_tc': 'AQAAANaf5BmyqAAAiBq/PD71bidj43Wr', 'u_asec': '099%23KAFEGYEKEmEEhYTLEEEEEpEQz0yFD6DHDrsMQ6DTDXnIW6tcSryID60FDEFETcZdt9TXE7EFbOR5D3QTEEx6zIywjYFETrZtt3illuYTEHITmobEjUYOI8KNZIYvzaD4kLjBaaLN6wEW6oSq33Nny8gN1EGGP8TC01Tqiwhv0JmSoVbIXXg4kLNowDN7VNGrykxyZBut3aIQqFCccyEaRJfrsEFEpcZdt3illuZdsyaDMlllszJP%2F3mrlllr%2BuZdtkslluWRsyaDy%2FllsUPoE7EIsyaD4hAw3Q9rE7EhssaZtfiMAYFEPOKXD67ScblcL4wsDRMBFEt6iRnpbOiRwEz6uz8nPvS7wSZuDahWFEPc929ibwUq%2BvD3Uoxkq7d71Sss4LpA97A6CsJKHai3woDWuVuCcYUqaMh%2BCapCL7FsLOaicpiRE9suQ6Yyh3Qs1bW4VsonaGhucsnKbOMFCGFuVfpMvw%2F6wEZtC21AaGA3wpw0zwIT%2FLbqzloVb4dq1QlzZjcSL4MR%2BO%2FkPOK6CGhudz2SrMe6iGr8sapX', 'pgv_si': 's61286400', 'pgv_pvi': '1071216640', 'aliyungf_tc': 'AQAAAOlGWTqRmAAAiBq/PGH9PUOiL1M7', 'LATELY_SHOPID': '1148601'}
 
-
     def parse(self, response):
         yunji_product_json_url = "https://m.yunjiweidian.com/yunjibuyer/listItemByCategoryLevel.json?pageSize=15&categoryLevelId1=27&categoryLevelId2=0&pageIndex="
         for i in range(0, 46):
